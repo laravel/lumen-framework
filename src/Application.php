07e1d09338c34c3083b86222e33e26ad0223a708
Taylor Otwell <taylorotwell@gmail.com>
2015-04-13 22:47:10 -0500

    Welcome screen.

--- src/Application.php
+++ src/Application.php
@@ -1558,4 +1558,61 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
         'view' => 'registerViewBindings',
         'Illuminate\Contracts\View\Factory' => 'registerViewBindings',
     ];
+
+    /**
+     * Get the HTML from the Lumen welcome screen.
+     *
+     * @return string
+     */
+    public function welcome()
+    {
+        return "<html>
+            <head>
+                <title>Lumen</title>
+
+                <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+
+                <style>
+                    body {
+                        margin: 0;
+                        padding: 0;
+                        width: 100%;
+                        height: 100%;
+                        color: #B0BEC5;
+                        display: table;
+                        font-weight: 100;
+                        font-family: 'Lato';
+                    }
+
+                    .container {
+                        text-align: center;
+                        display: table-cell;
+                        vertical-align: middle;
+                    }
+
+                    .content {
+                        text-align: center;
+                        display: inline-block;
+                    }
+
+                    .title {
+                        font-size: 96px;
+                        margin-bottom: 40px;
+                    }
+
+                    .quote {
+                        font-size: 24px;
+                    }
+                </style>
+            </head>
+            <body>
+                <div class=\"container\">
+                    <div class=\"content\">
+                        <div class=\"title\">Lumen.</div>
+                    </div>
+                </div>
+            </body>
+        </html>
+        ";
+    }
 }
