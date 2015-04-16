edd2db1e77cc788e915f329eb0052f2c1374255f
Phan Thanh Cong <ptcong90@gmail.com>
2015-04-16 10:43:42 +0700

    fix error 404 if not run Lumen in root [2]
    Previous PR, i have missing a case that if uri contains a path like sub-folder name

--- src/Application.php
+++ src/Application.php
@@ -1324,8 +1324,11 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
     public function getPathInfo()
     {
         $query = isset($_SERVER['QUERY_STRING']) ? $_SERVER['QUERY_STRING'] : '';
-        $uri = str_replace(dirname($_SERVER['SCRIPT_NAME']).'/', '', $_SERVER['REQUEST_URI']);
-
+        $folder = dirname($_SERVER['SCRIPT_NAME']).'/';
+        $uri = $_SERVER['REQUEST_URI'];
+        if (strpos($uri, $folder) === 0) {
+            $uri = substr($uri, strlen($folder));
+        }
         return '/'.ltrim(str_replace('?'.$query, '', $uri), '/');
     }
 
