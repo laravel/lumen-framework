813c113ac29bf377c11b1fa56b9cdd7444c777a7
Taylor Otwell <taylorotwell@gmail.com>
2015-04-27 09:47:43 -0500

    fix request binding

--- src/Application.php
+++ src/Application.php
@@ -1066,6 +1066,9 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
     public function dispatch($request = null)
     {
         if ($request) {
+            $this->instance('Illuminate\Http\Request', $request);
+            $this->ranServiceBinders['registerRequestBindings'] = true;
+
             $method = $request->getMethod();
             $pathInfo = $request->getPathInfo();
         } else {
