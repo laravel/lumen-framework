ea186f49e5840fb39eb45eb0b37b4959dc3575ca
Michael Wright <michael@wserver.co.uk>
2015-05-07 10:10:26 +0100

    Add missing Event Facade

--- src/Application.php
+++ src/Application.php
@@ -768,6 +768,7 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
             class_alias('Illuminate\Support\Facades\DB', 'DB');
             class_alias('Illuminate\Support\Facades\Cache', 'Cache');
             class_alias('Illuminate\Support\Facades\Crypt', 'Crypt');
+            class_alias('Illuminate\Support\Facades\Event', 'Event');
             class_alias('Illuminate\Support\Facades\Hash', 'Hash');
             class_alias('Illuminate\Support\Facades\Log', 'Log');
             class_alias('Illuminate\Support\Facades\Mail', 'Mail');
