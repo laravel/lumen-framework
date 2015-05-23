926a274eab0e45f235a89919648c86e639c955c5
yozman <yozman@sina.com>
2015-05-23 17:08:56 +0800

    fix route match bug
    route match will missed in some urlencode cases

--- src/Application.php
+++ src/Application.php
@@ -1360,7 +1360,9 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
     {
         $query = isset($_SERVER['QUERY_STRING']) ? $_SERVER['QUERY_STRING'] : '';
 
-        return '/'.trim(str_replace('?'.$query, '', $_SERVER['REQUEST_URI']), '/');
+        $path = '/'.trim(str_replace('?'.$query, '', $_SERVER['REQUEST_URI']), '/');
+
+        return rawurldecode($path);
     }
 
     /**
