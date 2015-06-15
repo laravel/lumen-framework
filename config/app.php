570ca74386ade9c40cb6da975bcdd9cd8403d714
Taylor Otwell <taylorotwell@gmail.com>
2015-06-15 15:13:05 -0500

    change cipher

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
 
     'key' => env('APP_KEY', 'SomeRandomString!!!'),
 
-    'cipher' => MCRYPT_RIJNDAEL_128,
+    'cipher' => 'AES-256-CBC',
 
     /*
     |--------------------------------------------------------------------------
