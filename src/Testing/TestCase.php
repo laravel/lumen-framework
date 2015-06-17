f8787fb5ffb1120d9da138b3eb19351f0bb06d36
jonnybarnes <jonnybarnes@gmail.com>
2015-06-17 21:54:51 +0100

    Remove Mockery from the Laravel\Lumen\Testing namespace

--- src/Testing/TestCase.php
+++ src/Testing/TestCase.php
@@ -63,7 +63,7 @@ abstract class TestCase extends PHPUnit_Framework_TestCase
     public function tearDown()
     {
         if (class_exists('Mockery')) {
-            Mockery::close();
+            \Mockery::close();
         }
 
         if ($this->app) {
