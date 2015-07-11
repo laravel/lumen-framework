6ffdd760376a65e9fcc3b4c0a74fd9165cd5f460
Bradley Suira <bradleysuiram@gmail.com>
2015-07-11 14:52:15 -0500

    Custom name of cookie for session
    Add the option to set a custom name of the cookie for session in the environment file configuration, by default accept the name of 'laravel_session'
    
      'cookie' => env('SESSION_DRIVER', 'laravel_session'),

--- config/session.php
+++ config/session.php
@@ -109,7 +109,7 @@ return [
     |
     */
 
-    'cookie' => 'laravel_session',
+    'cookie' => env('SESSION_DRIVER', 'laravel_session'),
 
     /*
     |--------------------------------------------------------------------------
