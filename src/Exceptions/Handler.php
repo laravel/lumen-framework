f62a4ae896e08acaa5edd14ea31ffeb43e93f582
Graham Campbell <graham@alt-three.com>
2015-08-03 13:04:16 +0100

    Update the exception handler to work on symfony 3

--- src/Exceptions/Handler.php
+++ src/Exceptions/Handler.php
@@ -1,7 +1,9 @@
 <?php namespace Laravel\Lumen\Exceptions;
 
 use Exception;
+use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Contracts\Debug\ExceptionHandler;
+use Symfony\Component\Debug\Exception\FlattenException;
 use Symfony\Component\Console\Application as ConsoleApplication;
 use Symfony\Component\Debug\ExceptionHandler as SymfonyExceptionHandler;
 
@@ -67,7 +69,41 @@ class Handler implements ExceptionHandler
      */
     public function render($request, Exception $e)
     {
-        return (new SymfonyExceptionHandler(env('APP_DEBUG', false)))->createResponse($e);
+        $e = FlattenException::create($e);
+        $handler = new SymfonyExceptionHandler(env('APP_DEBUG', false));
+        $dectorated = $this->decorate($handler->getContent($e), $handler->getStylesheet($e));
+
+        return Response::create($dectorated, $e->getStatusCode(), $e->getHeaders());
+    }
+
+    /**
+     * Get the html response content.
+     *
+     * @param  string  $content
+     * @param  string  $css
+     * @return string
+     */
+    private function decorate($content, $css)
+    {
+        return <<<EOF
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="robots" content="noindex,nofollow" />
+        <style>
+            /* Copyright (c) 2010, Yahoo! Inc. All rights reserved. Code licensed under the BSD License: http://developer.yahoo.com/yui/license.html */
+            html{color:#000;background:#FFF;}body,div,dl,dt,dd,ul,ol,li,h1,h2,h3,h4,h5,h6,pre,code,form,fieldset,legend,input,textarea,p,blockquote,th,td{margin:0;padding:0;}table{border-collapse:collapse;border-spacing:0;}fieldset,img{border:0;}address,caption,cite,code,dfn,em,strong,th,var{font-style:normal;font-weight:normal;}li{list-style:none;}caption,th{text-align:left;}h1,h2,h3,h4,h5,h6{font-size:100%;font-weight:normal;}q:before,q:after{content:'';}abbr,acronym{border:0;font-variant:normal;}sup{vertical-align:text-top;}sub{vertical-align:text-bottom;}input,textarea,select{font-family:inherit;font-size:inherit;font-weight:inherit;}input,textarea,select{*font-size:100%;}legend{color:#000;}
+            html { background: #eee; padding: 10px }
+            img { border: 0; }
+            #sf-resetcontent { width:970px; margin:0 auto; }
+            $css
+        </style>
+    </head>
+    <body>
+        $content
+    </body>
+</html>
+EOF;
     }
 
     /**
