5e649d2545f0a67d44a662cf5f46f959f678583c
Alexey <butochnikov@gmail.com>
2015-08-31 15:12:54 +0300

    Update Application.php

--- src/Application.php
+++ src/Application.php
@@ -1746,11 +1746,14 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
                 <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
 
                 <style>
+                    html, body {
+                        height: 100%;
+                    }
+                    
                     body {
                         margin: 0;
                         padding: 0;
                         width: 100%;
-                        height: 100%;
                         color: #B0BEC5;
                         display: table;
                         font-weight: 100;
