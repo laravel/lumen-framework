e14d8d441c380835212b09509c5fecea46d34a07
Jasper Koers <jasper@koers.biz>
2015-09-11 16:34:31 +0200

    Set DB_CONNECTION env variable to Queue failed default database

--- config/queue.php
+++ config/queue.php
@@ -87,7 +87,7 @@ return [
     */
 
     'failed' => [
-        'database' => 'mysql', 'table' => 'failed_jobs',
+        'database' => env('DB_CONNECTION', 'mysql'), 'table' => 'failed_jobs',
     ],
 
 ];
