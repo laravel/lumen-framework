895a42ee26bd2079fde85e5d4b0dbf9335f96e9f
Taylor Otwell <taylorotwell@gmail.com>
2015-12-02 14:45:31 -0600

    change version to 5.2

--- src/Application.php
+++ src/Application.php
@@ -100,7 +100,7 @@ class Application extends Container
      */
     public function version()
     {
-        return 'Lumen (6.0.0-dev) (Laravel Components 5.2.*)';
+        return 'Lumen (5.2.0-dev) (Laravel Components 5.2.*)';
     }
 
     /**
