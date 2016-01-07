41c7b2cedbd476a4949db6be84ae8655585f707e
Taylor Otwell <taylorotwell@gmail.com>
2016-01-07 17:11:47 -0600

    increment version

--- src/Application.php
+++ src/Application.php
@@ -108,7 +108,7 @@ class Application extends Container
      */
     public function version()
     {
-        return 'Lumen (5.2.0) (Laravel Components 5.2.*)';
+        return 'Lumen (5.2.1) (Laravel Components 5.2.*)';
     }
 
     /**
