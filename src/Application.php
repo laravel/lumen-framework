3a556ddb2df9097dd31cf7f187c88a5b3cfc27fa
Mohamed Said <themohamedsaid@gmail.com>
2016-09-06 17:09:38 +0200

    update version

--- src/Application.php
+++ src/Application.php
@@ -111,7 +111,7 @@ class Application extends Container
      */
     public function version()
     {
-        return 'Lumen (5.2.8) (Laravel Components 5.2.*)';
+        return 'Lumen (5.3) (Laravel Components 5.3.*)';
     }
 
     /**
