8d4107d449cb225b22f0ea0ebc67bac70c5dd64f
Ardy Satria Hasanuddin <forzadraco@gmail.com>
2016-09-30 21:55:54 +0700

    Missing trans function

--- src/helpers.php
+++ src/helpers.php
@@ -345,3 +345,23 @@ if (! function_exists('view')) {
         return $factory->make($view, $data, $mergeData);
     }
 }
+
+if (! function_exists('trans')) {
+    /**
+     * Translate the given message.
+     *
+     * @param  string  $id
+     * @param  array   $parameters
+     * @param  string  $domain
+     * @param  string  $locale
+     * @return string
+     */
+    function trans($id = null, $parameters = [], $domain = 'messages', $locale = null)
+    {
+        if (is_null($id)) {
+            return app('translator');
+        }
+
+        return app('translator')->trans($id, $parameters, $domain, $locale);
+    }
+}
