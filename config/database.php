c229bb34fd0a68d805455e36196a65e6ef494523
Yang Liu <myly10@users.noreply.github.com>
2016-11-25 23:17:55 +0800

    Adding support for Unix socket of MySQL

--- config/database.php
+++ config/database.php
@@ -60,6 +60,7 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
+            'unix_socket'      => env('UNIX_SOCKET', ''),
             'port'      => env('DB_PORT', 3306),
             'database'  => env('DB_DATABASE', 'forge'),
             'username'  => env('DB_USERNAME', 'forge'),
