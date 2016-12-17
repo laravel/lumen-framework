2019c5b5e59b5ba1da521107e049d62f99ac44a0
Jan Hartigan <janhartigan13@gmail.com>
2016-12-17 11:34:53 -0800

    calling phpunit asserter statically

--- src/Testing/Concerns/MakesHttpRequests.php
+++ src/Testing/Concerns/MakesHttpRequests.php
@@ -183,7 +183,7 @@ trait MakesHttpRequests
             json_decode($this->response->getContent(), true)
         ));
 
-        $this->assertEquals(json_encode(array_sort_recursive($data)), $actual);
+        PHPUnit::assertEquals(json_encode(array_sort_recursive($data)), $actual);
 
         return $this;
     }
@@ -198,7 +198,7 @@ trait MakesHttpRequests
     public function seeJson(array $data = null, $negate = false)
     {
         if (is_null($data)) {
-            $this->assertJson(
+            PHPUnit::assertJson(
                 $this->response->getContent(), "JSON was not returned from [{$this->currentUri}]."
             );
 
@@ -238,16 +238,16 @@ trait MakesHttpRequests
 
         foreach ($structure as $key => $value) {
             if (is_array($value) && $key === '*') {
-                $this->assertInternalType('array', $responseData);
+                PHPUnit::assertInternalType('array', $responseData);
 
                 foreach ($responseData as $responseDataItem) {
                     $this->seeJsonStructure($structure['*'], $responseDataItem);
                 }
             } elseif (is_array($value)) {
-                $this->assertArrayHasKey($key, $responseData);
+                PHPUnit::assertArrayHasKey($key, $responseData);
                 $this->seeJsonStructure($structure[$key], $responseData[$key]);
             } else {
-                $this->assertArrayHasKey($value, $responseData);
+                PHPUnit::assertArrayHasKey($value, $responseData);
             }
         }
 
@@ -268,7 +268,7 @@ trait MakesHttpRequests
         $actual = json_decode($this->response->getContent(), true);
 
         if (is_null($actual) || $actual === false) {
-            return $this->fail('Invalid JSON was returned from the route. Perhaps an exception was thrown?');
+            return PHPUnit::fail('Invalid JSON was returned from the route. Perhaps an exception was thrown?');
         }
 
         $actual = json_encode(array_sort_recursive(
@@ -278,7 +278,7 @@ trait MakesHttpRequests
         foreach (array_sort_recursive($data) as $key => $value) {
             $expected = $this->formatToExpectedJson($key, $value);
 
-            $this->{$method}(
+            call_user_func(['PHPUnit_Framework_Assert', $method],
                 Str::contains($actual, $expected),
                 ($negate ? 'Found unexpected' : 'Unable to find')." JSON fragment [{$expected}] within [{$actual}]."
             );
@@ -427,10 +427,10 @@ trait MakesHttpRequests
     {
         $headers = $this->response->headers;
 
-        $this->assertTrue($headers->has($headerName), "Header [{$headerName}] not present on response.");
+        PHPUnit::assertTrue($headers->has($headerName), "Header [{$headerName}] not present on response.");
 
         if (! is_null($value)) {
-            $this->assertEquals(
+            PHPUnit::assertEquals(
                 $headers->get($headerName), $value,
                 "Header [{$headerName}] was found, but value [{$headers->get($headerName)}] does not match [{$value}]."
             );
