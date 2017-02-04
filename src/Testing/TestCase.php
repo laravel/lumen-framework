86b0f16b8639b7dffa20c9fc26cf7efffbcf53f8
Graham Campbell <graham@alt-three.com>
2017-02-04 15:59:15 +0000

    Use the same phpunit base class as laravel 5.4

--- src/Testing/TestCase.php
+++ src/Testing/TestCase.php
@@ -4,11 +4,11 @@ namespace Laravel\Lumen\Testing;
 
 use Mockery;
 use Exception;
-use PHPUnit_Framework_TestCase;
+use PHPUnit\Framework\TestCase as BaseTestCase;
 use Illuminate\Support\Facades\Facade;
 use Illuminate\Contracts\Auth\Authenticatable;
 
-abstract class TestCase extends PHPUnit_Framework_TestCase
+abstract class TestCase extends BaseTestCase
 {
     use Concerns\MakesHttpRequests;
 
