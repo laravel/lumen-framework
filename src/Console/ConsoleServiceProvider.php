ef47bfe3a864e1b7f5b6ae669497c208f303191c
Mulia Arifandi Nasution <mul14.net@gmail.com>
2017-12-06 15:16:54 +0700

    Add migrate:fresh command
    Signed-off-by: Mulia Arifandi Nasution <mul14.net@gmail.com>

--- src/Console/ConsoleServiceProvider.php
+++ src/Console/ConsoleServiceProvider.php
@@ -21,6 +21,7 @@ use Illuminate\Queue\Console\ListFailedCommand as ListFailedQueueCommand;
 use Illuminate\Queue\Console\FlushFailedCommand as FlushFailedQueueCommand;
 use Illuminate\Queue\Console\ForgetFailedCommand as ForgetFailedQueueCommand;
 use Illuminate\Database\Console\Migrations\ResetCommand as MigrateResetCommand;
+use Illuminate\Database\Console\Migrations\FreshCommand as MigrateFreshCommand;
 use Illuminate\Database\Console\Migrations\StatusCommand as MigrateStatusCommand;
 use Illuminate\Database\Console\Migrations\InstallCommand as MigrateInstallCommand;
 use Illuminate\Database\Console\Migrations\RefreshCommand as MigrateRefreshCommand;
@@ -46,6 +47,7 @@ class ConsoleServiceProvider extends ServiceProvider
         'ClearResets' => 'command.auth.resets.clear',
         'Migrate' => 'command.migrate',
         'MigrateInstall' => 'command.migrate.install',
+        'MigrateFresh' => 'command.migrate.fresh',
         'MigrateRefresh' => 'command.migrate.refresh',
         'MigrateReset' => 'command.migrate.reset',
         'MigrateRollback' => 'command.migrate.rollback',
@@ -190,6 +192,18 @@ class ConsoleServiceProvider extends ServiceProvider
             $composer = $app['composer'];
 
             return new MigrateMakeCommand($creator, $composer);
+        });
+    }
+
+    /**
+     * Register the command.
+     *
+     * @return void
+     */
+    protected function registerMigrateFreshCommand()
+    {
+        $this->app->singleton('command.migrate.fresh', function () {
+            return new MigrateFreshCommand;
         });
     }
 
