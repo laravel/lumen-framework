1995d20719d25eed32d13f503b7fcc47d783e2e5
BranchZero Sun <branchzero@gmail.com>
2018-02-22 11:25:52 +0800

    Keep validate method return only with rule items

--- src/Routing/ProvidesConvenienceMethods.php
+++ src/Routing/ProvidesConvenienceMethods.php
@@ -3,6 +3,7 @@
 namespace Laravel\Lumen\Routing;
 
 use Closure as BaseClosure;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Validation\Validator;
@@ -64,7 +65,21 @@ trait ProvidesConvenienceMethods
             $this->throwValidationException($request, $validator);
         }
 
-        return $validator->getData();
+        return $this->extractInputFromRules($request, $rules);
+    }
+
+    /**
+     * Get the request input based on the given validation rules.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  array  $rules
+     * @return array
+     */
+    protected function extractInputFromRules(Request $request, array $rules)
+    {
+        return $request->only(collect($rules)->keys()->map(function ($rule) {
+            return Str::contains($rule, '.') ? explode('.', $rule)[0] : $rule;
+        })->unique()->toArray());
     }
 
     /**
