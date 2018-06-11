47777bb8a93e8a5494907cc488490bd79c06f61c
Carlos Del Rosario <carlos.d@irokopartners.com>
2018-06-11 15:25:58 -0400

    Fixes condition to json_decode responseData

--- src/Testing/Concerns/MakesHttpRequests.php
+++ src/Testing/Concerns/MakesHttpRequests.php
@@ -237,7 +237,7 @@ trait MakesHttpRequests
             return $this->seeJson();
         }
 
-        if (! $responseData) {
+        if (!is_array($responseData)) {
             $responseData = json_decode($this->response->getContent(), true);
         }
 
