37f1e4a50650acf4c7bb8b754a8801e4741c4994
Taylor Otwell <taylor@laravel.com>
2018-09-10 14:46:53 -0500

    version

--- src/Application.php
+++ src/Application.php
@@ -133,7 +133,7 @@ class Application extends Container
      */
     public function version()
     {
-        return 'Lumen (5.7-dev) (Laravel Components 5.7.*)';
+        return 'Lumen (5.7.0) (Laravel Components 5.7.*)';
     }
 
     /**
