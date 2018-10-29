b2bc401c5c2e2b2064dafff6d3a357becc7d3919
Dries Vints <dries.vints@gmail.com>
2018-10-29 14:40:33 +0100

    Version

--- src/Application.php
+++ src/Application.php
@@ -133,7 +133,7 @@ class Application extends Container
      */
     public function version()
     {
-        return 'Lumen (5.7.3) (Laravel Components 5.7.*)';
+        return 'Lumen (5.7.4) (Laravel Components 5.7.*)';
     }
 
     /**
