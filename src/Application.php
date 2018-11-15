18e279b710aabab6a03559e3f15e0a65fc4eb848
Dries Vints <dries.vints@gmail.com>
2018-11-15 18:05:57 +0100

    Version
    Signed-off-by: Dries Vints <dries.vints@gmail.com>

--- src/Application.php
+++ src/Application.php
@@ -133,7 +133,7 @@ class Application extends Container
      */
     public function version()
     {
-        return 'Lumen (5.7.5) (Laravel Components 5.7.*)';
+        return 'Lumen (5.7.6) (Laravel Components 5.7.*)';
     }
 
     /**
