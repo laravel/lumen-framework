24bf37937bc60c10b96a1cdd8f56219b9fc56604
Mior Muhammad Zaki <crynobone@gmail.com>
2019-01-04 11:31:02 +0800

    [5.8] Remove deprecated methods.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Routing/UrlGenerator.php
+++ src/Routing/UrlGenerator.php
@@ -183,30 +183,6 @@ class UrlGenerator
     }
 
     /**
-     * Get the scheme for a raw URL.
-     *
-     * @param  bool|null  $secure
-     * @return string
-     * @deprecated v5.5.x
-     */
-    protected function getScheme($secure)
-    {
-        return $this->formatScheme($secure);
-    }
-
-    /**
-     * Force the schema for URLs.
-     *
-     * @param  string  $schema
-     * @return void
-     * @deprecated v5.5.x
-     */
-    public function forceSchema($schema)
-    {
-        $this->forceScheme($schema);
-    }
-
-    /**
      * Force the schema for URLs.
      *
      * @param  string  $schema
