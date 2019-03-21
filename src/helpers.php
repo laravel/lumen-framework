62e55acddf29b70dc31436a65ed1ec277b1cd956
Dries Vints <dries.vints@gmail.com>
2019-03-21 18:09:48 +0100

    Re-add PutEnvAdapter

--- src/helpers.php
+++ src/helpers.php
@@ -5,6 +5,7 @@ use Illuminate\Container\Container;
 use Dotenv\Environment\DotenvFactory;
 use Laravel\Lumen\Bus\PendingDispatch;
 use Illuminate\Contracts\Bus\Dispatcher;
+use Dotenv\Environment\Adapter\PutenvAdapter;
 use Dotenv\Environment\Adapter\EnvConstAdapter;
 use Dotenv\Environment\Adapter\ServerConstAdapter;
 
@@ -160,7 +161,7 @@ if (! function_exists('env')) {
         static $variables;
 
         if ($variables === null) {
-            $variables = (new DotenvFactory([new EnvConstAdapter, new ServerConstAdapter]))->createImmutable();
+            $variables = (new DotenvFactory([new EnvConstAdapter, new ServerConstAdapter, PutenvAdapter::class]))->createImmutable();
         }
 
         return Option::fromValue($variables->get($key))
