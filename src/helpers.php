363c7a0d85754a3dd05915beb700d120a2319a83
Graham Campbell <graham@alt-three.com>
2019-06-21 14:09:14 +0100

    Don't redefine env, it's already in illuminate/support

--- src/helpers.php
+++ src/helpers.php
@@ -148,48 +148,6 @@ if (! function_exists('encrypt')) {
     }
 }
 
-if (! function_exists('env')) {
-    /**
-     * Gets the value of an environment variable.
-     *
-     * @param  string  $key
-     * @param  mixed   $default
-     * @return mixed
-     */
-    function env($key, $default = null)
-    {
-        static $variables;
-
-        if ($variables === null) {
-            $variables = (new DotenvFactory([new EnvConstAdapter, new PutenvAdapter, new ServerConstAdapter]))
-                ->createImmutable();
-        }
-
-        return Option::fromValue($variables->get($key))
-            ->map(function ($value) {
-                switch (strtolower($value)) {
-                    case 'true':
-                    case '(true)':
-                        return true;
-                    case 'false':
-                    case '(false)':
-                        return false;
-                    case 'empty':
-                    case '(empty)':
-                        return '';
-                    case 'null':
-                    case '(null)':
-                        return;
-                }
-
-                return $value;
-            })
-            ->getOrCall(function () use ($default) {
-                return value($default);
-            });
-    }
-}
-
 if (! function_exists('event')) {
     /**
      * Dispatch an event and call the listeners.
