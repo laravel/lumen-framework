3d759fbc130f90ce3643080fc158f5ef7a93818d
Lin07ux <linshengli.linux7@icloud.com>
2019-10-21 11:11:07 +0800

    fix: make the faker_option in app config effective

--- src/Application.php
+++ src/Application.php
@@ -364,6 +364,9 @@ class Application extends Container
     protected function registerDatabaseBindings()
     {
         $this->singleton('db', function () {
+            // For Faker\Generator
+            $this->configure('app');
+
             return $this->loadComponent(
                 'database', [
                     'Illuminate\Database\DatabaseServiceProvider',
