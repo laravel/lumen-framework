dd854ca13c0ccbb6e0ccc97560ed284c2e50cf86
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2020-04-30 14:44:00 +0100

    Fixed call to FatalError constructor

--- src/Concerns/RegistersExceptionHandlers.php
+++ src/Concerns/RegistersExceptionHandlers.php
@@ -77,7 +77,7 @@ trait RegistersExceptionHandlers
      */
     protected function fatalErrorFromPhpError(array $error, $traceOffset = null)
     {
-        return new FatalError($error['message'], $error, $traceOffset);
+        return new FatalError($error['message'], 0, $error, $traceOffset);
     }
 
     /**
