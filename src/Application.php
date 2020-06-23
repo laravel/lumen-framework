5205e9965a61339a2cedbf3795fbed70446f15a3
Taylor Otwell <taylorotwell@gmail.com>
2020-06-23 10:40:08 -0500

    version

--- src/Application.php
+++ src/Application.php
@@ -162,7 +162,7 @@ class Application extends Container
      */
     public function version()
     {
-        return 'Lumen (7.1.4) (Laravel Components ^7.0)';
+        return 'Lumen (7.2.0) (Laravel Components ^7.0)';
     }
 
     /**
