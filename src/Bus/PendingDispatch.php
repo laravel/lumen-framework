c1fa1b52255d849d09f57320168b17bfafa86178
brandformers <40883156+brandformers@users.noreply.github.com>
2021-06-23 22:52:16 +0200

    Added shouldDispatch() to PendingDispatch.php

--- src/Bus/PendingDispatch.php
+++ src/Bus/PendingDispatch.php
@@ -2,7 +2,10 @@
 
 namespace Laravel\Lumen\Bus;
 
+use Illuminate\Container\Container;
 use Illuminate\Contracts\Bus\Dispatcher;
+use Illuminate\Contracts\Cache\Repository as Cache;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
 
 class PendingDispatch
 {
@@ -51,12 +54,41 @@ class PendingDispatch
     }
 
     /**
+     * Determine if the job should be dispatched.
+     *
+     * @return bool
+     */
+    protected function shouldDispatch()
+    {
+        if (! $this->job instanceof ShouldBeUnique) {
+            return true;
+        }
+
+        $uniqueId = method_exists($this->job, 'uniqueId')
+                    ? $this->job->uniqueId()
+                    : ($this->job->uniqueId ?? '');
+
+        $cache = method_exists($this->job, 'uniqueVia')
+                    ? $this->job->uniqueVia()
+                    : Container::getInstance()->make(Cache::class);
+
+        return (bool) $cache->lock(
+            $key = 'laravel_unique_job:'.get_class($this->job).$uniqueId,
+            $this->job->uniqueFor ?? 0
+        )->get();
+    }
+
+    /**
      * Handle the object's destruction.
      *
      * @return void
      */
     public function __destruct()
     {
-        app(Dispatcher::class)->dispatch($this->job);
+        if (! $this->shouldDispatch()) {
+            return;
+        } else {
+            app(Dispatcher::class)->dispatch($this->job);
+        }
     }
 }
