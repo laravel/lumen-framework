0bf8dab20c6398ba873e0bdc20ab35026d62314d
Dries Vints <dries@vints.io>
2022-02-15 15:19:33 +0100

    [9.x] Lumen v9 prep (#1218)
    * Update validation lang file
    
    * Skeleton changes

--- config/logging.php
+++ config/logging.php
@@ -77,10 +77,11 @@ return [
         'papertrail' => [
             'driver' => 'monolog',
             'level' => 'debug',
-            'handler' => SyslogUdpHandler::class,
+            'handler' => env('LOG_PAPERTRAIL_HANDLER', SyslogUdpHandler::class),
             'handler_with' => [
                 'host' => env('PAPERTRAIL_URL'),
                 'port' => env('PAPERTRAIL_PORT'),
+                'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT'),
             ],
         ],
 
