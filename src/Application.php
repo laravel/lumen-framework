703d677301d3760c3427039d842807bf95c039ba
Taylor Otwell <taylor@laravel.com>
2026-03-16 08:54:58 -0500

    Update Application.php

--- src/Application.php
+++ src/Application.php
@@ -519,6 +519,7 @@ class Application extends Container
     {
         $this->singleton(LoggerInterface::class, function () {
             $this->register(ContextServiceProvider::class);
+
             $this->configure('logging');
 
             return new LogManager($this);
